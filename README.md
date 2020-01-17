b5863fd2c2cce161358ac915e60110e8003a3115
WooMin <hanulbom@gmail.com>
2020-01-17 15:34:43 +0900

    #2.7 Express Core: Middlewares

--- README.md
+++ README.md
@@ -1,12 +1,39 @@
 # Wetube
 Cloning Youtube with Vanilla and NodeJS
-"" bash
-$npm install 
-""
 
+
+## Installation
+
+``` bash
+$ npm install 
+```
+
+### Babel
+
+``` bash
 $ npm install @babel/node
 $ npm install @babel/preset-env
 $ npm install @babel/core
+```
 
---
+### Initialize
+
+``` bash
 npm install nodemon -D
+```
+
+``` bash
+npm install morgan
+```
+
+``` bash
+npm install helmet
+```
+
+``` bash
+npm install body-parser
+```
+
+``` bash
+npm install cookie-parser
+```
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express()
 
 const PORT = 4000;
@@ -15,11 +19,18 @@ const betweenHome = (req, res, next) => {
   next();
 }
 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(helmet());
+app.use(morgan("dev"));
 
-app.get("/", handleHome);
+const middleware = (req, res, next) => {
+  res.send("not happening");
+}
+
+app.get("/", middleware, handleHome);
 
 app.get("/profile", handleProfile);
-
-app.use(betweenHome);
 
 app.listen(PORT, handleListening);

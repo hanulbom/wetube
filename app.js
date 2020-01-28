1eaade6caf45de97267e12eeaa7008e84774b30b
WooMin <hanulbom@gmail.com>
2020-01-28 11:12:10 +0900

    #2.10 routes

--- app.js
+++ app.js
@@ -3,17 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 const app = express()
-
-const handleHome = (req, res) => res.send('Hello from home');
-
-const handleProfile = (req, res) => res.send('You are on my profile');
-
-const betweenHome = (req, res, next) => {
-  console.log("I'm between");
-  next();
-}
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -21,14 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-const middleware = (req, res, next) => {
-  res.send("not happening");
-}
-
-app.get("/", middleware, handleHome);
-
-app.get("/profile", handleProfile);
-
-app.get("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;

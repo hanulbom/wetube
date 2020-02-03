53c931ba29cdfeb7eb6653f6f499e40859a30d8c
WooMin <hanulbom@gmail.com>
2020-02-03 10:42:04 +0900

    #2.16 Local Variables in Pug

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -5,10 +5,10 @@ import { join, login, logout } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.home, home)
-globalRouter.get(routes.search, search)
-globalRouter.get(routes.join, join)
-globalRouter.get(routes.login, login)
-globalRouter.get(routes.logout, logout)
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.search, search);
+globalRouter.get(routes.join, join);
+globalRouter.get(routes.login, login);
+globalRouter.get(routes.logout, logout);
 
 export default globalRouter;
--- routes.js
+++ routes.js
@@ -1,14 +1,14 @@
 // Global
 const HOME = "/";
 const JOIN = "/join";
-const LOGIN = "login";
+const LOGIN = "/login";
 const LOGOUT = "/logout";
 const SEARCH = "/search";
 
 // Users
 
 const USERS = "/users";
-const USERS_DETAIL = "/:id";
+const USER_DETAIL = "/:id";
 const EDIT_PROFILE = "/edit-profile";
 const CHANGE_PASSWORD = "/change-password";
 
@@ -17,25 +17,24 @@ const CHANGE_PASSWORD = "/change-password";
 const VIDEOS = "/videos";
 const UPLOAD = "/upload";
 const VIDEO_DETAIL = "/:id";
-const EDIT_VIDEO = "/:id/eidt";
+const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
 
-
 const routes = {
-    home: HOME,
-    join: JOIN,
-    login: LOGIN,
-    logout: LOGOUT,
-    search: SEARCH,
-    users: USERS,
-    userDetail: USERS_DETAIL,
-    editProfile: EDIT_PROFILE,
-    changePassword: CHANGE_PASSWORD,
-    videos: VIDEOS,
-    upload: UPLOAD,
-    videoDetail: VIDEO_DETAIL,
-    editVideo: EDIT_VIDEO,
-    deleteVideo: DELETE_VIDEO
+  home: HOME,
+  join: JOIN,
+  login: LOGIN,
+  logout: LOGOUT,
+  search: SEARCH,
+  users: USERS,
+  userDetail: USER_DETAIL,
+  editProfile: EDIT_PROFILE,
+  changePassword: CHANGE_PASSWORD,
+  videos: VIDEOS,
+  upload: UPLOAD,
+  videoDetail: VIDEO_DETAIL,
+  editVideo: EDIT_VIDEO,
+  deleteVideo: DELETE_VIDEO
 };
 
 export default routes;

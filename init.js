2d3194977391b60deef495d7a0bc61c9451a9e24
WooMin <hanulbom@gmail.com>
2020-03-10 15:43:20 +0900

    #3.1 Connecting to MongoDB

--- init.js
+++ init.js
@@ -1,7 +1,8 @@
+import { videos } from "./db";
 import app from "./app";
 
 const PORT = 4000;
 
-const handleListening = () => console.log(`😁Lisning on: http://localhost:${PORT}`);
+const handleListening = () => console.log(`✅  Lisning on: http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);

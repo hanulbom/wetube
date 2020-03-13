14e76c0d87fbffaf0bac7559e2621015b9ddf6e6
WooMin <hanulbom@gmail.com>
2020-03-13 17:15:33 +0900

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,41 +2,41 @@ const path = require("path");
 const autoprefixer = require("autoprefixer");
 const ExtractCSS = require("extract-text-webpack-plugin");
 
-const MODE = process.env.WABPACK_ENV;
-const ENTRY_FILE = path.resolve(_dirname, "assets", "js", "main.js");
-const OUTPUT_DIR = path.join(_dirname, "static");
+const MODE = process.env.WEBPACK_ENV;
+const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
+const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
   entry: ENTRY_FILE,
   mode: MODE,
   module: {
-      rules: [
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: ExtractCSS.extract([
           {
-              test: /\(.scss)$/,
-              use: ExtractCSS.extract([
-                  {
-                      loader: "css-loader"
-                  },
-                  {
-                      loader: "postcss-loader",
-                      options: {
-                          plugin() {
-                              return [autoprefixer({ browsers: "cover 99.5%" })];
-                          }
-                      }
-                  },
-                  {
-                      loader: "sass-loader"
-                  }
-              ])
+            loader: "css-loader"
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugin() {
+                return [autoprefixer({ browsers: "cover 99.5%" })];
+              }
+            }
+          },
+          {
+            loader: "sass-loader"
           }
-      ]
-  }
+        ])
+      }
+    ]
+  },
   output: {
     path: OUTPUT_DIR,
-    filename: "[name].[format]"
+    filename: "[name].js"
   },
   plugins: [new ExtractCSS("styles.css")]
 };
 
-module.export = config;
+module.exports = config;

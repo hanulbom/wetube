cceb2ebf637df61625446ac8492ad063ccd692ba
WooMin <hanulbom@gmail.com>
2020-03-13 14:44:22 +0900

    fix

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -45,29 +45,29 @@ export const videoDetail = async (req, res) => {
     }
 };
 
-export const editVideo = async (req, res) => {
+export const getEditVideo = async (req, res) => {
     const {
-        params: {id}
+      params: { id }
     } = req;
     try {
-        const video = await Video.findById(id);
-        res.render("editVideo", {pageTitle: `Edit ${video.title}`, video});
+      const video = await Video.findById(id);
+      res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
     } catch (error) {
-        res.redirect(routes.home);
+      res.redirect(routes.home);
     }
-};
-
-export const postEditVideo = async (req, res) => {
+  };
+  
+  export const postEditVideo = async (req, res) => {
     const {
-        params: { id }
-        body: {title, description}
+      params: { id },
+      body: { title, description }
     } = req;
     try {
-        await Video.findOneAndUpdate({ id }, {title, description });
-        res.redirect(routes.videoDetail(id));
+      await Video.findOneAndUpdate({ id }, { title, description });
+      res.redirect(routes.videoDetail(id));
     } catch (error) {
-        res.redirect(routes.home);
+      res.redirect(routes.home);
     }
-};
+  };
 
 export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });

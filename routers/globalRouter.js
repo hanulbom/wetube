a383891439b7c0cdf7a95b5b96561e55b5f96059
WooMin <hanulbom@gmail.com>
2020-03-17 13:52:56 +0900

    fix

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -23,7 +23,7 @@ globalRouter.post(routes.login, onlyPublic, postLogin);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.logout, onlyPublic, logout);
+globalRouter.get(routes.logout, logout);
 
 globalRouter.get(routes.gitHub, githubLogin);
 

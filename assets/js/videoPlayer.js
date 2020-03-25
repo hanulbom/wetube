da3e4d45672638025dcf7ccc11861e016b9a4c0e
WooMin <hanulbom@gmail.com>
2020-03-25 17:26:24 +0900

    #8.5 Volume Bar part One

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -74,7 +74,7 @@ const formatDate = seconds => {
 };
 
 function getCurrentTime() {
-  currentTime.innerHTML = formatDate(videoPlayer.currentTime);
+  currentTime.innerHTML = formatDate(Mate.floor(videoPlayer.currentTime));
 }
 
 function setTotalTime() {
@@ -83,11 +83,17 @@ function setTotalTime() {
   setInterval(getCurrentTime, 1000);
 }
 
+function handleEnded() {
+  videoPlayer.currentTime = 0;
+  playBtn.innerHTML = '<i class="fas fa-play"></i>';
+}
+
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
   volumeBtn.addEventListener("click", handleVolumeClick);
   fullScrnBtn.addEventListener("click", goFullScreen);
   setTotalTime();
+  videoPlayer.addEventListener("ended", handleEnded);
 }
 
 if (videoContainer) {
